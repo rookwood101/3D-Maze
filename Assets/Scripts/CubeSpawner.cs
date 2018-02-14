5daa8081438d1ec5a186ef1f2b396f2c5d2d8dec
rookwood101 <alexandersadler1@gmail.com>
2018-02-14 16:02:57 +0000

    Add a cube factory which spawns one cube a frame, forever

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Beginning Cube Spawning");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate (cube);
+	}
+}
