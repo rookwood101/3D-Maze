7ee765a4b93f89a9a0b1428dd927d30ad7115886
Jean-Luc Mourey <moureyjeanluc@gmail.com>
2018-02-15 19:59:09 +0000

    Deleted factory, optimized rotation code with quaternions

--- Assets/Scripts/MazeRotator.cs
+++ Assets/Scripts/MazeRotator.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class MazeRotator : MonoBehaviour {
 
-    //public float horizontalSpeed = 2.0F;
-    //public float verticalSpeed = 2.0F;
     private float oldMouseX, oldMouseY, newMouseX, newMouseY;
-    private Vector3 cursorDelta;
+    private Vector3 cursorDelta, intoCube;
     private float rotaAxisX, rotaAxisY, rotaAxisZ; //Axis of Quaternion rotation
     private float relativeAxisX, relativeAxisY, relativeAxisZ; //adjusted axies to simulate cube having 0, 0, 0 rotation
 
@@ -17,40 +15,38 @@ public class MazeRotator : MonoBehaviour {
         newMouseX = Input.mousePosition.x;
         newMouseY = Input.mousePosition.y;
         if (Input.GetMouseButton(0)) {
-            cursorDelta = new Vector3 (newMouseX-oldMouseX, newMouseY - oldMouseY, 0.0f);
-
+            
             /*
             mouseDelta.x -= transform.eulerAngles.x;
             mouseDelta.y -= transform.eulerAngles.y;
             mouseDelta.z -= transform.eulerAngles.z;
             Vector3 adjustedCross = Vector3.Cross(mouseDelta, intoCube);
             */
+            
             //gets rotation axis from Cross of mouseDelta Vector and Camera vision direction vector
-            Vector3 intoCube = new Vector3(0, 0, 1);
-            Quaternion rotate = Quaternion.AngleAxis(1, Vector3.Cross(cursorDelta, intoCube)); //- Quaternion.identity;
+            cursorDelta = new Vector3(newMouseX - oldMouseX, newMouseY - oldMouseY, 0.0f);
+            intoCube = new Vector3(0, 0, 1);
+            transform.rotation *= Quaternion.AngleAxis(1, Vector3.Cross(cursorDelta, intoCube));
 
+            /*
             //gets rotation vector for maze
             rotaAxisX = rotate.eulerAngles.x;
             rotaAxisY = rotate.eulerAngles.y;
             rotaAxisZ = rotate.eulerAngles.z;
             Vector3 rotationAxis = new Vector3(rotaAxisX, rotaAxisY, rotaAxisZ);
             transform.Rotate(rotationAxis); //rotate
-        } else {
+            transform.rotation = transform.rotation + rotate;
+            */
+
+        }
+        /*
+        else {
             relativeAxisX = transform.eulerAngles.x;
             relativeAxisY = transform.eulerAngles.y;
             relativeAxisZ = transform.eulerAngles.z;
         }
+        */
         oldMouseX = newMouseX;
         oldMouseY = newMouseY;
-
-        
-
-
-
-        /* THIS ONLY WORKS IN A SINGLE DIRECTION
-        float h = horizontalSpeed * Input.GetAxis("Mouse X");
-        float v = verticalSpeed * Input.GetAxis("Mouse Y");
-        transform.Rotate(v, h, 0);
-        */
     }
 }
