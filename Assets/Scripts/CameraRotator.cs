4c16973fd552809d0e5745562e87fc0212ac61ba
Jean-Luc Mourey <moureyjeanluc@gmail.com>
2018-02-18 18:00:03 +0000

    Made modifications and added TODO comments

--- Assets/Scripts/CameraRotator.cs
+++ Assets/Scripts/CameraRotator.cs
@@ -5,16 +5,48 @@ using UnityEngine;
 public class CameraRotator : MonoBehaviour {
 
 	[SerializeField]
-	private int rotationSpeed;
+    int rotationSpeed = 100;
+	[SerializeField]
+	private GameObject cameraRotator;
+	[SerializeField]
+	private GameObject camera;
+
+    private bool camPosSaved = false;
+    private Quaternion savedCamPos;
+    private float oldMouseX, oldMouseY, newMouseX, newMouseY;
+    private Vector3 cursorDelta, intoCube;
 
 	// Use this for initialization
 	void Start () {
-		
+        //savedCamPos = camera.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.UpArrow)) {
+
+        //gets the position differential of the cursor between frames to calculate rotation of maze
+        newMouseX = Input.mousePosition.x;
+        newMouseY = Input.mousePosition.y;
+        if (Input.GetMouseButton(0))
+        {
+            if (camPosSaved == false) {
+                //savedCamPos = camera.transform.rotation;
+                camPosSaved = true;
+            }
+            //gets rotation axis from Cross of mouseDelta Vector and Camera vision direction vector
+            cursorDelta = new Vector3(newMouseX - oldMouseX, newMouseY - oldMouseY, 0.0f);
+
+            intoCube = cameraRotator.transform.position - camera.transform.position;
+            transform.rotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.Cross(cursorDelta, intoCube));
+        } else {
+            //camera.transform.rotation = savedCamPos;
+            camPosSaved = false;
+        }
+        oldMouseX = newMouseX;
+        oldMouseY = newMouseY;
+
+        /*
+        if (Input.GetKey(KeyCode.UpArrow)) {
 			transform.rotation = transform.rotation * Quaternion.Euler (rotationSpeed * Time.deltaTime, 0, 0);
 		}
 		if (Input.GetKey(KeyCode.DownArrow)) {
@@ -26,5 +58,11 @@ public class CameraRotator : MonoBehaviour {
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			transform.rotation = Quaternion.Euler (0, rotationSpeed * Time.deltaTime, 0) * transform.rotation;
 		}
-	}
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        */
+    }
+
+
 }
--- Assets/Scripts/MazeRotator.cs
+++ Assets/Scripts/MazeRotator.cs
@@ -4,21 +4,54 @@ using UnityEngine;
 
 public class MazeRotator : MonoBehaviour {
 
-	[SerializeField]
-    int rotationSpeed = 100;
-	[SerializeField]
-	private GameObject cameraRotator;
-	[SerializeField]
-	private GameObject camera;
+    [SerializeField]
+    int rotationSpeed;
 
-    private float oldMouseX, oldMouseY, newMouseX, newMouseY;
-    private Vector3 cursorDelta, intoCube;
-    private float rotaAxisX, rotaAxisY, rotaAxisZ; //Axis of Quaternion rotation
-    private float relativeAxisX, relativeAxisY, relativeAxisZ; //adjusted axies to simulate cube having 0, 0, 0 rotation
+    private bool rotating = false;
+    private IEnumerator coroutine;
+    public bool isTop, isBot, isRight, isLeft, isFront, isBack;
+    
+    void Update() {
+        //TODO make cube rotate depending on which side the ball is on
+        //if (Quaternion.LookRotation )
 
-    void Update()
-    {
+        //TODO make cube tilt correctly, at this point only W and A work
+        if (Input.GetKey(KeyCode.W) && (transform.eulerAngles.x % 90) < 15)
+        {
+            transform.rotation *= Quaternion.Euler(rotationSpeed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.S) && (transform.eulerAngles.x % 90) < 15)
+        {
+            transform.rotation *= Quaternion.Euler(-rotationSpeed * Time.deltaTime, 0, 0);
+        }
+        else if ((transform.eulerAngles.x % 90) > 0) {
+            //TODO rotate gradually back to x = 0
+        }
 
+        if (Input.GetKey(KeyCode.A) && (transform.eulerAngles.z % 90) < 15) {
+            transform.rotation *= Quaternion.Euler(0, 0, rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) && (transform.eulerAngles.z % 90) < 15) {
+            transform.rotation *= Quaternion.Euler(0, 0, -rotationSpeed * Time.deltaTime);
+        }
+        else if ((transform.eulerAngles.x % 90) > 0) {
+            //TODO rotate gradually back to z = 0
+        }
+
+        //makes cube rotate 90°, but also moves ball, so either find a way to make
+        //this work in CameraRotator or freeze ball physics while rotating == true
+        if (Input.GetKeyDown(KeyCode.Q) && !rotating)
+        {
+            coroutine = WaitForSeconds(0.01f, 1);
+            StartCoroutine(coroutine);
+        }
+        if (Input.GetKeyDown(KeyCode.E) && !rotating)
+        {
+            coroutine = WaitForSeconds(0.01f, -1);
+            StartCoroutine(coroutine);
+        }
+
+        /*
         //gets the position differential of the cursor between frames to calculate rotation of maze
         newMouseX = Input.mousePosition.x;
         newMouseY = Input.mousePosition.y;
@@ -33,5 +66,15 @@ public class MazeRotator : MonoBehaviour {
 
         oldMouseX = newMouseX;
         oldMouseY = newMouseY;
+        */
+    }
+
+    private IEnumerator WaitForSeconds(float delay, int direction) {
+        rotating = true;
+        for (int i = 0; i < 45; i++) {
+            yield return new WaitForSeconds(delay);
+            transform.rotation *= Quaternion.AngleAxis(direction * 2, new Vector3(0, 1, 0));
+        }
+        rotating = false;
     }
 }
