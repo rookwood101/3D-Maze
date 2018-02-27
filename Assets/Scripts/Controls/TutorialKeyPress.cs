fb0c920565e88296036f0771ea67b686a78fd5f4
Magnarokk <Jean-Luc@Jean-Lucs-MacBook-Pro.local>
2018-02-27 15:08:00 +0000

    Added WASD Tutorial components and script as well as Q and E arrows

---
+++ Assets/Scripts/Controls/TutorialKeyPress.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialKeyPress : MonoBehaviour {
+
+	[SerializeField]
+	private KeyCode myKey;
+	private Renderer rend;
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+
+
+	void Update () {
+		if (Input.GetKeyDown(myKey)) {
+			Fade ();
+		}
+	}
+
+	void Fade() {
+		if (rend.material.color.a > 0) {
+			Color color = rend.material.color;
+			color.a -= 0.1f;
+			rend.material.color = color;
+			Invoke ("Fade", 0.05f);
+		}
+	}
+}
